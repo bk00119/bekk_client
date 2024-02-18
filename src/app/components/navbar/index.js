8602fa85ee75ce9b23298c6fb675a1825b90647d
kaitlynchau <kaitlynachau@gmail.com>
2024-02-18 12:31:08 -0500

    complete navbar w/ logo

--- src/app/components/navbar/index.js
+++ src/app/components/navbar/index.js
@@ -1,14 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
-// import logo from "@public/logo.png"
+import logo from "@public/logo.png"
 
 export default function Navbar() {
   return (
-    <nav className="bg-violet flex justify-center h-24 items-center  px-4">
+    <nav className="bg-violet flex justify-between h-24 items-center px-4">
       <div className="w-7xl">
         <Link href="/">
-          {/* <Image src={logo} alt="logo" height={48} priority /> */}
-          LOGO
+          <Image src={logo} alt="logo" height={100} priority />
         </Link>
       </div>
     </nav>
