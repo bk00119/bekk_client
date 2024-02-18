c1067d84af979871b914618fbf98748f2f893b53
kaitlynchau <kaitlynachau@gmail.com>
2024-02-18 13:00:32 -0500

    added sign up sign in buttons/ref to navbar

--- src/app/components/navbar/index.js
+++ src/app/components/navbar/index.js
@@ -4,10 +4,18 @@ import logo from "@public/logo.png"
 
 export default function Navbar() {
   return (
-    <nav className="bg-violet flex justify-between h-24 items-center px-4">
+    <nav className="bg-violet flex justify-between h-24 items-center px-4 md:px-8">
       <div className="w-7xl">
         <Link href="/">
           <Image src={logo} alt="logo" height={100} priority />
+        </Link>
+      </div>
+      <div>
+        <Link href="/signup" className =  "inline-block rounded-full px-4 py-2 bg-gray-300 text-gray-800 mr-4">
+          Sign Up
+        </Link>
+        <Link href="/signin">
+          Sign In
         </Link>
       </div>
     </nav>
