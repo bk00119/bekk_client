1030c2d6a74f15114c0393630208dff6b1fe9352
kaitlynchau <kaitlynachau@gmail.com>
2024-02-25 21:54:28 -0500

    local and cloud scripts

--- cloud.sh
+++ cloud.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This shell script deploys a new version to a server.
-export REACT_APP_BACKEND_DOMAIN=https://bk00119.pythonanywhere.com
+# This shell script deploys a new version of the react application to a server.
+export REACT_APP_BACKEND_URL=https://bk00119.pythonanywhere.com
 npm start
---
+++ local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This shell script runs the server locally.
+export REACT_APP_BACKEND_URL=http://localhost:3000
+#starts development server
+npm start
