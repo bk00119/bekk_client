33d6af01127f7cbfcc586c517c1b72e06d9908c3
kaitlynchau <kaitlynachau@gmail.com>
2024-03-31 19:41:57 -0400

    now generates task_id when successful

--- src/app/api/task/create/route.js
+++ src/app/api/task/create/route.js
@@ -1,14 +1,13 @@
 export async function POST(req) {
-    //const reqData = await req.json()
+    const reqData = await req.json()
+
     const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/postTask`, {
        
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        _id: '6575033f3b89d2b4f309d7af'
-      })
+      body: JSON.stringify(reqData)
     })
   
     const data = await res.json()
