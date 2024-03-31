640410cb7382011c8919bb737f0227136e4798a0
kaitlynchau <kaitlynachau@gmail.com>
2024-03-31 19:30:55 -0400

    create api and connect, trying now to resolve internal server error

---
+++ src/app/api/task/create/route.js
@@ -0,0 +1,17 @@
+export async function POST(req) {
+    //const reqData = await req.json()
+    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/postTask`, {
+       
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        _id: '6575033f3b89d2b4f309d7af'
+      })
+    })
+  
+    const data = await res.json()
+  
+    return Response.json(data)
+}
