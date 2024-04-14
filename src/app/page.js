dda9f050f554aaf795f5a233c45ad5f55e9054a5
kaitlynchau <kaitlynachau@gmail.com>
2024-04-14 15:18:22 -0400

    fixed cards and page displaying

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ async function getFeed() {
   // CURRENT USER'S ID
   const curr_user_id = access_token_data?.user_id
 
-  console.log(curr_user_id)
+
   if(!curr_user_id) return console.log("Error: failed loading feed bc user_id ")
 
   const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/view/posts/${curr_user_id}`, {
@@ -23,68 +23,10 @@ async function getFeed() {
   return res.json()
 }
 
-// export default async function Home() {
-//   const feed = await getFeed()
-//   var json = JSON.stringify(feed)
-//   json = JSON.parse(json)
-//   console.log(json)
-
-
-
-
-
-//   // return (
-//   //   <div className="w-full">
-//   //     <h1 className="text-2xl mb-8">Feed</h1>
-//   //     {feed.Tasks ? (
-//   //       Object.entries(feed.Tasks).map(([key, value]) => (
-//   //         <div key={key}>
-//   //           <FeedCard task_id={key} task_data={value} />
-//   //         </div>
-//   //       ))
-//   //     ) : (
-//   //       <ErrorMessage message="Error: failed loading feed" />
-//   //     )}
-//   //   </div>
-//   // )
-
-//   return (
-//     // <div className="w-full">
-//     //   <h1 className="text-2xl mb-8">Feed</h1>
-//     //    {feed && feed.length > 0 ? (
-//     //     feed.map((post, index) => (
-//     //       <div key={index}>
-//     //         {/* Assuming post_id is present in each post object */}
-//     //         <FeedCard post_id={post.post_id} post_data={post} />
-//     //       </div>
-//     //     ))
-//     //   ) : (
-//     //     <ErrorMessage message="Error: failed loading feed" />
-//     //   )}
-//     // </div>
-//     <div className="w-full">
-//       <h1 className="text-2xl mb-8">Feed</h1>
-//         <h2>{json.content}</h2>
-//     </div>
-      
-      
-    
-//   );
-  
-// }
 export default async function Home() {
   const feed = await getFeed();
-  
-  // Check if feed exists and has at least one item
+
   if (feed && Object.keys(feed).length > 0) {
-    // Get the first key from the feed object
-    const feedKeys = Object.keys(feed);
-    const firstFeedKey = feedKeys[0];
-    
-    // Access the content property of the first object in the feed object
-    const content = feed[firstFeedKey].content;
-    console.log(content); // Output the content
-    
     return (
       <div className="w-full">
         <h1 className="text-2xl mb-8">Feed</h1>
